430a26e5a340b26b0de56ec0de029520233505bd
Dorothy Rifai <drifai28@gmail.com>
2021-09-05 21:32:51 -0400

    unit tests for queryPost and createPost working

---
+++ src/test/java/com/drifai/graphqlsdl/resolver/post/PostMutationResolverTest.java
@@ -0,0 +1,46 @@
+package com.drifai.graphqlsdl.resolver.post;
+
+import com.drifai.graphqlsdl.TestApplication;
+import com.drifai.graphqlsdl.dto.AuthorDto;
+import com.drifai.graphqlsdl.service.AuthorService;
+import com.graphql.spring.boot.test.GraphQLResponse;
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TestApplication.class)
+public class PostMutationResolverTest {
+
+    @Autowired
+    GraphQLTestTemplate graphQLTestTemplate;
+
+    @MockBean
+    AuthorService authorServiceMock;
+
+    static UUID uuid = UUID.randomUUID();
+
+    @Test
+    public void shouldCreateAuthor() throws IOException, JSONException {
+        AuthorDto author = AuthorDto.builder()
+                .email("test@gmail.com")
+                .name("test")
+                .build();
+        doReturn(uuid).when(authorServiceMock).createAuthor(author);
+        GraphQLResponse graphQLResponse = graphQLTestTemplate.postForResource("request/create-author-mutation.graphqls");
+        assertEquals(graphQLResponse.isOk(), true);
+        String uuid = graphQLResponse.get("$.data.createAuthor");
+        assertTrue(uuid != null);
+     }
+
+}
---
+++ src/test/java/com/drifai/graphqlsdl/resolver/post/PostQueryResolverTest.java
@@ -0,0 +1,28 @@
+package com.drifai.graphqlsdl.resolver.post;
+
+import com.drifai.graphqlsdl.FileReaderUtil;
+import com.drifai.graphqlsdl.TestApplication;
+import com.graphql.spring.boot.test.GraphQLResponse;
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TestApplication.class)
+class PostQueryResolverTest {
+
+    @Autowired
+    GraphQLTestTemplate graphQLTestTemplate;
+
+    @Test
+    public void shouldGetPostData() throws IOException, JSONException {
+        GraphQLResponse graphQLResponse = graphQLTestTemplate.postForResource("request/post-query.graphqls");
+        assertEquals(graphQLResponse.isOk(), true);
+        assertEquals(FileReaderUtil.removeWhiteSpace(FileReaderUtil.read("response/post-query.json")),FileReaderUtil.removeWhiteSpace(graphQLResponse.getRawResponse().getBody()));
+    }
+}
