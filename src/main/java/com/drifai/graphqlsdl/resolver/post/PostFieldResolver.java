5bfc0853c86e1bd8c4851f350c897b8f9c38b90d
Dorothy Rifai <drifai28@gmail.com>
2021-09-21 07:38:38 -0400

    Async field resolver example

--- src/main/java/com/drifai/graphqlsdl/resolver/post/PostFieldResolver.java
+++ src/main/java/com/drifai/graphqlsdl/resolver/post/PostFieldResolver.java
@@ -7,28 +7,49 @@ import com.drifai.graphqlsdl.dto.AuthorDto;
 import com.drifai.graphqlsdl.dto.PostDto;
 import com.drifai.graphqlsdl.service.AuthorService;
 import com.drifai.graphqlsdl.service.PostService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+@Slf4j
 @Component
 public class PostFieldResolver implements GraphQLResolver<PostDto> {
 
     private final AuthorService authorService;
-    private final PostService postService;
     private final CommentService commentService;
 
-    public PostFieldResolver(AuthorService authorService, PostService postService, CommentService commentService) {
+    private final ExecutorService executorService = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public PostFieldResolver(AuthorService authorService, CommentService commentService) {
         this.authorService = authorService;
-        this.postService = postService;
         this.commentService = commentService;
     }
 
-    public AuthorDto getAuthor(PostDto postDto) {
-        return authorService.getAuthorById(postDto.getAuthorId());
+    public CompletableFuture<AuthorDto> getAuthor(PostDto postDto) {
+        log.info("Author main thread id: {}", Thread.currentThread().getId());
+        return CompletableFuture.supplyAsync(() -> {
+          log.info("Author thread id: {}", Thread.currentThread().getId());
+          log.info("Author request started for postId:{}.", postDto.getId());
+          AuthorDto authorById = authorService.getAuthorById(postDto.getAuthorId());
+          log.info("Author request completed for postId:{}.", postDto.getId());
+          return authorById;
+        }, executorService);
      }
 
-    public List<CommentDto> getComments(PostDto postDto, Integer first) {
-        return commentService.getFirstNCommentByPostId(postDto.getId(), first);
+    public CompletableFuture<List<CommentDto>> getComments(PostDto postDto, Integer first) {
+        log.info("Comment main thread id: {}", Thread.currentThread().getId());
+        return CompletableFuture.supplyAsync(() -> {
+          log.info("Comment thread id: {}", Thread.currentThread().getId());
+          log.info("Comment request started for postId:{}.", postDto.getId());
+          List<CommentDto> list = commentService.getFirstNCommentByPostId(postDto.getId(), first);
+          log.info("Comment request ended for postId:{}.", postDto.getId());
+          return list;
+        }, executorService);
     }
 }
